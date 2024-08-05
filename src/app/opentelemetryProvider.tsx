81c53fd274d9f6e9d35ee6e9ccaf48b77e45e35f
simon-bai <simon.bai@aelfscan.io>
2024-08-05 19:37:14 +0800

    fix: fix nfts Inventory link

--- src/app/opentelemetryProvider.tsx
+++ src/app/opentelemetryProvider.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from 'react';
 import { initWebTracerWithZone } from 'opentelemetry-launcher';
 import { createContext, useEffect, useMemo, useState } from 'react';
 import { Tracer } from '@opentelemetry/sdk-trace-base';
+import { useEnvContext } from 'next-runtime-env';
+import { checkMainNet } from '@_utils/isMainNet';
 
 interface Props {
   readonly children: ReactNode;
@@ -12,14 +14,17 @@ interface Props {
 export const OpentelemetryContext = createContext<Tracer | undefined>(undefined);
 
 export const OpentelemetryProvider = ({ children, config }: Props) => {
+  const { NEXT_PUBLIC_NETWORK_TYPE } = useEnvContext();
+  const isMainNet = checkMainNet(NEXT_PUBLIC_NETWORK_TYPE);
+  const prefix = isMainNet ? 'mainnet' : 'testnet';
   const [webTracerWithZone, setWebTracerWithZone] = useState<Tracer>();
   const APP_SETTINGS = useMemo(() => {
     return {
       openTelemetry: {
-        serviceName: 'aelfscan-web',
+        serviceName: 'aelfscan-web-' + 'prefix',
         serviceVersion: 'v1.0',
         collectorEndpoint: config.collectorEndpoint,
-        tracerName: 'aelfscan-web-tracer',
+        tracerName: `aelfscan-web-${prefix}-tracer`,
         ignoreUrls: [
           /\/sockjs-node/,
           /\/monitoring/,
@@ -30,7 +35,7 @@ export const OpentelemetryProvider = ({ children, config }: Props) => {
         propagateTraceHeaderCorsUrls: ['https://httpbin.org'],
       },
     };
-  }, [config.collectorEndpoint]);
+  }, [config.collectorEndpoint, prefix]);
 
   useEffect(() => {
     const _webTracerWithZone = initWebTracerWithZone(APP_SETTINGS.openTelemetry);
